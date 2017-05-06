ad711a40d47c9d604fcf31b06921a5eee289ba3d
ronan <ronan_mccabe@protonmail.com>
2017-05-07 01:41:18 +0300

    Remove genserver, use tasks.

--- lib/gitgit.ex
+++ lib/gitgit.ex
@@ -3,50 +3,37 @@ defmodule Gitgit do
   Scraping github profiles from gitter.
   """
 
-  use GenServer
+  @room_id Application.get_env(:gitgit, :gitter_room)
+  @token Application.get_env(:gitgit, :gitter_token)
+  @headers ["Authorization": "Bearer #{@token}"]
 
-  @name GW
-
-  ## Client API
-
-  def start_link(opts \\ []) do
-    GenServer.start_link(__MODULE__, :ok, opts ++ [name: GW])
+  def split100(x) do
+    range = 0..(div x, 100)
+    Enum.map(range, fn x -> x * 100 end)
   end
 
-  def get_usernames do
-    GenServer.call(@name, {:usernames})
+  def get_room do
+    "https://api.gitter.im/v1/rooms/#{@room_id}" |> HTTPoison.get(@headers) |> parse_response
   end
 
-  def add_results(data) do
-    GenServer.call(@name, {:add_results, data})
+  def get_users(offset) do
+    result =  "https://api.gitter.im/v1/rooms/#{@room_id}/users?limit=100&skip=#{offset}" |> HTTPoison.get(@headers) |> parse_response
+    case result do
+      {:ok, res} ->
+        res
+      {:error, reason} ->
+        IO.puts "ERROR: #{inspect reason}"
+        []
+    end
+
   end
 
-  def stop do
-    GenServer.cast(@name, :stop)
+  defp parse_response({:ok, %HTTPoison.Response{body: body, status_code: 200}}) do
+    body |> JSON.decode
   end
 
-  ## Server Callbacks
-
-  def init(:ok) do
-    {:ok, []}
-  end
-
-  def handle_call({:add_results, data}, _from, state) do
-    {:reply, :ok, data ++ state}
-  end
-
-  def handle_call({:usernames}, _from, state) do
-    {:reply, Gitgit.Helpers.go, state}
-  end
-
-  def handle_cast(:stop, state) do
-    {:stop, :normal, state}
-  end
-
-  def terminate(reason, state) do
-    IO.puts "server terminated because of #{inspect reason}"
-    File.write("data.json", JSON.encode!(state))
-    :ok
+  defp parse_response(_) do
+    { :error, "oops" }
   end
 
 end
--- lib/go.exs
+++ lib/go.exs
@@ -1,2 +1,27 @@
-Gitgit.start_link
-Gitgit.get_usernames
+case Gitgit.get_room do
+
+  {:ok, json} ->
+    tasks =
+      json["userCount"]
+      |> Gitgit.split100
+      |> Enum.map(fn offset ->
+           Task.async(fn -> Gitgit.get_users(offset) end)
+         end)
+
+    data =
+      tasks
+      |> Task.yield_many
+      |> Enum.map(fn {task, res} ->
+           res || Task.shutdown(task, :brutal_kill)
+         end)
+      |> Enum.map(fn {:ok, res} -> res end)
+      |> List.flatten
+      |> Enum.map(&(Map.get(&1, "username")))
+      |> JSON.encode!
+
+    File.write("data.json", data)
+
+  {:error, reason} ->
+    "ERROR: #{inspect reason}"
+
+end
