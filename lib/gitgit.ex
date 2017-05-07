e1fcbaad8127a47f90989a6f4e9dbe2bc7daa1a0
ronan <ronan_mccabe@protonmail.com>
2017-05-07 15:40:53 +0300

    Add github requests and debraid gitter room id.

--- lib/gitgit.ex
+++ lib/gitgit.ex
@@ -3,29 +3,48 @@ defmodule Gitgit do
   Scraping github profiles from gitter.
   """
 
-  @room_id Application.get_env(:gitgit, :gitter_room)
-  @token Application.get_env(:gitgit, :gitter_token)
-  @headers ["Authorization": "Bearer #{@token}"]
+  @gitter_headers ["Authorization": "Bearer #{Application.get_env(:gitgit, :gitter_token)}"]
+  @github_headers ["Authorization": "token #{Application.get_env(:gitgit, :github_token)}"]
 
   def split100(x) do
     range = 0..(div x, 100)
     Enum.map(range, fn x -> x * 100 end)
   end
 
-  def get_room do
-    "https://api.gitter.im/v1/rooms/#{@room_id}" |> get_json
+  def get_gitter_room(room_id) do
+    "https://api.gitter.im/v1/rooms/#{room_id}" |> gitter_get
   end
 
-  def get_users(offset) do
-    "https://api.gitter.im/v1/rooms/#{@room_id}/users?limit=100&skip=#{offset}" |> get_json
+  def get_gitter_room_members(room_id, offset) do
+    "https://api.gitter.im/v1/rooms/#{room_id}/users?limit=100&skip=#{offset}" |> gitter_get
   end
 
-  defp get_json(url) do
-    case HTTPoison.get!(url, @headers) do
+  def get_github_profile(username) do
+    case HTTPoison.get!("https://api.github.com/users/#{username}", @github_headers) do
+      %{body: body, status_code: 200} ->
+        body |> JSON.decode!
+      %{status_code: 404} ->
+        %{}
+      _ ->
+        raise "github request error"
+    end
+  end
+
+  def yield_and_parse_tasks(tasks) do
+    tasks
+    |> Task.yield_many
+    |> Enum.map(fn {task, res} ->
+         res || Task.shutdown(task, :brutal_kill)
+       end)
+    |> Enum.map(fn {:ok, res} -> res end)
+  end
+
+  defp gitter_get(url) do
+    case HTTPoison.get!(url, @gitter_headers) do
       %{body: body, status_code: 200} ->
         body |> JSON.decode!
       _ ->
-        raise "request error"
+        raise "gitter request error"
     end
   end
 
--- lib/go.exs
+++ lib/go.exs
@@ -1,21 +1,24 @@
-%{ "userCount" => userCount } = Gitgit.get_room
+room_id = Application.get_env(:gitgit, :gitter_room)
 
-tasks =
+%{ "userCount" => userCount } = Gitgit.get_gitter_room(room_id)
+
+usernames =
   userCount
   |> Gitgit.split100
   |> Enum.map(fn offset ->
-       Task.async(fn -> Gitgit.get_users(offset) end)
+       Task.async(fn -> Gitgit.get_gitter_room_members(room_id, offset) end)
      end)
-
-data =
-  tasks
-  |> Task.yield_many
-  |> Enum.map(fn {task, res} ->
-       res || Task.shutdown(task, :brutal_kill)
-     end)
-  |> Enum.map(fn {:ok, res} -> res end)
+  |> Gitgit.yield_and_parse_tasks
   |> List.flatten
   |> Enum.map(&(Map.get(&1, "username")))
+
+profiles =
+  usernames
+  |> Enum.map(fn username ->
+       Task.async(fn -> Gitgit.get_github_profile(username) end)
+     end)
+  |> Gitgit.yield_and_parse_tasks
+  |> Enum.map(&(Map.take(&1, ["name", "location", "email", "company", "login"])))
   |> JSON.encode!
 
-File.write("data.json", data)
+File.write("data.json", profiles)
