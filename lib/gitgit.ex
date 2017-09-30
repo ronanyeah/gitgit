d580b5a4ad62af2f59cb22fba99ebb57b7b48357
ronan <ronan_mccabe@protonmail.com>
2017-09-30 01:05:47 +0100

    refactor

--- lib/gitgit.ex
+++ lib/gitgit.ex
@@ -6,17 +6,39 @@ defmodule Gitgit do
   @gitter_headers ["Authorization": "Bearer #{Application.get_env(:gitgit, :gitter_token)}"]
   @github_headers ["Authorization": "token #{Application.get_env(:gitgit, :github_token)}"]
 
+  @doc """
+  Splits integer in increments of 100 for rate limited API requests.
+
+  ## Examples
+
+      iex> Gitgit.split100(432)
+      [0, 100, 200, 300, 400]
+  """
   def split100(x) do
     0..(div x, 100)
     |> Enum.map(&(&1 * 100))
   end
 
   def get_gitter_room(room_id) do
-    "https://api.gitter.im/v1/rooms/#{room_id}" |> gitter_get
+    url = "https://api.gitter.im/v1/rooms/#{room_id}"
+
+    case HTTPoison.get!(url, @gitter_headers) do
+      %{status_code: 200, body: body} ->
+        body |> JSON.decode!
+      _ ->
+        raise "gitter request error"
+    end
   end
 
   def get_gitter_room_members(room_id, offset) do
-    "https://api.gitter.im/v1/rooms/#{room_id}/users?limit=100&skip=#{offset}" |> gitter_get
+    url = "https://api.gitter.im/v1/rooms/#{room_id}/users?limit=100&skip=#{offset}"
+
+    case HTTPoison.get!(url, @gitter_headers) do
+      %{status_code: 200, body: body} ->
+        body |> JSON.decode!
+      _ ->
+        raise "gitter request error"
+    end
   end
 
   def get_github_profile(username) do
@@ -27,15 +49,6 @@ defmodule Gitgit do
         %{}
       _ ->
         raise "github request error"
-    end
-  end
-
-  defp gitter_get(url) do
-    case HTTPoison.get!(url, @gitter_headers) do
-      %{status_code: 200, body: body} ->
-        body |> JSON.decode!
-      _ ->
-        raise "gitter request error"
     end
   end
 
